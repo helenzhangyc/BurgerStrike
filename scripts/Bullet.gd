f47bf751764c17214a749361b73d930a4678c3f5
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-07-08 12:30:16 -0400

    piercing bullet

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -3,21 +3,25 @@ class_name PlayerBullet extends Area2D
 @export var speed: int = 200
 @export var dir: Vector2 = Vector2(0, -1)
 
-@onready var shooter = load("res://scripts/Shooter.gd").new()
+# how many enemies can bullet hit before destroying itself
+@export var pierce: int = 1
 
-func set_dir(dir):
-	self.dir = dir
+var cur_pierce
+
+@onready var shooter = load("res://scripts/Shooter.gd").new()
 
 func _ready():
 	area_entered.connect(_on_area_entered)
+	cur_pierce = pierce
 
 func _physics_process(delta):
 	global_position += dir.rotated(rotation + PI/2) * speed * delta
 		
 func _on_area_entered(area):
-	print("entered", area.get_parent())
 	if area.get_parent() is Enemy:
-		queue_free()
+		cur_pierce -= 1
+		if cur_pierce <= 0:
+			queue_free()
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
